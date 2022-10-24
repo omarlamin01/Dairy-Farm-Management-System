2306ba3f9924c3d4759b499813cef6a4bc73bfb9
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-10-24 21:49:29 +0100

    add images directory in the resources

--- src/main/java/com/dfms/dairy_farm_management_system/HelloApplication.java
+++ src/main/java/com/dfms/dairy_farm_management_system/HelloApplication.java
@@ -11,8 +11,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("splash_screen.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setResizable(false);
+        stage.setTitle("Dairy Farm Management System");
         stage.setScene(scene);
         stage.show();
     }
