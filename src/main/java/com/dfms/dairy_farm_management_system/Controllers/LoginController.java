206fa49aa52c9c5190766505a7b66d84bae61dc8
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-10-25 22:34:26 +0100

    style the login page

--- src/main/java/com/dfms/dairy_farm_management_system/Controllers/LoginController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/Controllers/LoginController.java
@@ -1,4 +1,23 @@
 package com.dfms.dairy_farm_management_system.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML
+    private TextField email_input;
+
+    @FXML
+    private Label forget_password;
+
+    @FXML
+    private Button login_btn;
+
+    @FXML
+    private PasswordField password_input;
+
 }
