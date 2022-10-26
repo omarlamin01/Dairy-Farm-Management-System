53f4e089ffa25b7d7652c9b0fb403d37a72374ca
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-10-27 00:47:24 +0100

    !important

--- src/main/java/com/dfms/dairy_farm_management_system/Controllers/SplashScreenController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/Controllers/SplashScreenController.java
@@ -1,7 +1,5 @@
 package com.dfms.dairy_farm_management_system.Controllers;
 
-<<<<<<<< HEAD:src/main/java/com/dfms/dairy_farm_management_system/Controllers/SplashController.java
-import com.dfms.dairy_farm_management_system.HelloApplication;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,7 +13,6 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.stage.Window;
-========
 import com.dfms.dairy_farm_management_system.Main;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
@@ -23,24 +20,16 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
->>>>>>>> abdellatif:src/main/java/com/dfms/dairy_farm_management_system/Controllers/SplashScreenController.java
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-<<<<<<<< HEAD:src/main/java/com/dfms/dairy_farm_management_system/Controllers/SplashController.java
-public class SplashController implements Initializable {
-
-    @FXML
-    AnchorPane anchorPane;
-========
 public class SplashScreenController implements Initializable {
 
     private FXMLLoader fxmlLoader;
     private Stage stage;
     private Scene scene;
->>>>>>>> abdellatif:src/main/java/com/dfms/dairy_farm_management_system/Controllers/SplashScreenController.java
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -59,12 +48,9 @@ public class SplashScreenController implements Initializable {
                     @Override
                     public void run() {
                         try {
-<<<<<<<< HEAD:src/main/java/com/dfms/dairy_farm_management_system/Controllers/SplashController.java
                             // Switch to login
                             System.out.println("Switch to login");
-========
-                            switchToLoginPage();
->>>>>>>> abdellatif:src/main/java/com/dfms/dairy_farm_management_system/Controllers/SplashScreenController.java
+                            //  switchToLoginPage();
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -74,12 +60,19 @@ public class SplashScreenController implements Initializable {
         }.start();
     }
 
-<<<<<<<< HEAD:src/main/java/com/dfms/dairy_farm_management_system/Controllers/SplashController.java
-    public void switchScene(ActionEvent event) throws IOException {
-    }
-
-========
     private void switchToLoginPage() {
+        try {
+            fxmlLoader = new FXMLLoader(Main.class.getResource("login_screen.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            // get active stage
+            stage = (Stage) ((Node) fxmlLoader.getRoot()).getScene().getWindow();
+            stage.setTitle("Dairy Farm Management System");
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 //        fxmlLoader = new FXMLLoader(Main.class.getResource("login_screen.fxml"));
 //        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 //        try {
@@ -90,5 +83,5 @@ public class SplashScreenController implements Initializable {
 //        stage.setScene(scene);
 //        stage.show();
     }
->>>>>>>> abdellatif:src/main/java/com/dfms/dairy_farm_management_system/Controllers/SplashScreenController.java
+
 }
--- src/main/java/com/dfms/dairy_farm_management_system/Main.java
+++ src/main/java/com/dfms/dairy_farm_management_system/Main.java
@@ -3,6 +3,7 @@ package com.dfms.dairy_farm_management_system;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -10,11 +11,18 @@ import java.io.IOException;
 
 public class Main extends Application {
 
+    public static Stage getStage() {
+        //get current stage
+
+
+        return new Stage();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main_layout.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-//        stage.initStyle(StageStyle.UNDECORATED);
+        // stage.initStyle(StageStyle.UNDECORATED);
         stage.setTitle("Dairy Farm Management System");
         stage.setScene(scene);
         stage.show();
