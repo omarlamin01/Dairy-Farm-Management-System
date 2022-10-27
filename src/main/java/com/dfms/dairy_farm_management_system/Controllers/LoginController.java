0fb9fcb9d77743dc0ecd3d8d4fff189ee5625562
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-10-27 17:16:54 +0100

    swicth between scenes and bug fixes

--- src/main/java/com/dfms/dairy_farm_management_system/Controllers/LoginController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/Controllers/LoginController.java
@@ -1,14 +1,24 @@
 package com.dfms.dairy_farm_management_system.Controllers;
 
+import com.dfms.dairy_farm_management_system.Main;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
+    private FXMLLoader fxmlLoader;
+    private Stage stage;
+    private Scene scene;
 
     @FXML
     private Circle close_btn;
@@ -26,7 +36,21 @@ public class LoginController {
     private PasswordField password_input;
 
     @FXML
-    void exitApplication(MouseEvent event) {
+    private void login(MouseEvent event) {
+        //switch to main layout
+        fxmlLoader = new FXMLLoader(Main.class.getResource("main_layout.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        try {
+            scene = new Scene(fxmlLoader.load());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void exitApplication(MouseEvent event) {
         System.exit(0);
     }
 }
