50cbb4c7f61cb368d0724932d226877dc8170ddc
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-10-28 16:43:08 +0100

    update style

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/EmployeesController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/EmployeesController.java
@@ -1,9 +1,34 @@
 package com.dfms.dairy_farm_management_system.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.input.MouseEvent;
 
-public class EmployeesController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class EmployeesController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //initialize the combo box
+        ArrayList<String> exportOptions = new ArrayList<>();
+        exportOptions.add("Export to PDF");
+        exportOptions.add("Export to Excel");
+        exportOptions.add("Export to CSV");
+
+        exportComboBox.getItems().addAll(exportOptions);
+    }
+
+    @FXML
+    private ComboBox<String> exportComboBox;
+
+    @FXML
+    private Button openAddNewEmployeeBtn;
+
+
     @FXML
     public void openAddEmployee(MouseEvent mouseEvent) {
 
