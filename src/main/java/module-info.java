ce823bd7ece53a08c03ed4d0ba4c24c6b5b6dc55
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-10-28 11:11:19 +0100

    some packages name

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,6 @@ module com.dfms.dairy_farm_management_system {
 
     opens com.dfms.dairy_farm_management_system to javafx.fxml;
     exports com.dfms.dairy_farm_management_system;
-    exports com.dfms.dairy_farm_management_system.Controllers;
-    opens com.dfms.dairy_farm_management_system.Controllers to javafx.fxml;
+    exports com.dfms.dairy_farm_management_system.controllers;
+    opens com.dfms.dairy_farm_management_system.controllers to javafx.fxml;
 }
