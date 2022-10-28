ae2f68247d4da8ae5b6ce4a28ec8724e017df456
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-10-28 15:22:02 +0100

    update employees view

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/EmployeesController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/EmployeesController.java
@@ -1,4 +1,11 @@
 package com.dfms.dairy_farm_management_system.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
 public class EmployeesController {
+    @FXML
+    public void openAddEmployee(MouseEvent mouseEvent) {
+
+    }
 }
