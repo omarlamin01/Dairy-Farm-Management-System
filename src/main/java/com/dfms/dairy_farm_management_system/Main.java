c770fcad427ecc3bf67570104a3f8bd86172e690
Maryam Bouchhar <meryem.bouchhar@gmail.com>
2022-10-29 09:58:11 +0100

    the perfect input ui

--- src/main/java/com/dfms/dairy_farm_management_system/Main.java
+++ src/main/java/com/dfms/dairy_farm_management_system/Main.java
@@ -20,6 +20,7 @@ public class Main extends Application {
         stage.getIcons().add(new Image("file:src/main/resources/images/logo.png"));
         stage.setTitle("Dairy Farm Management System");
         stage.setScene(scene);
+        stage.setFullScreen(true);
         centerScreen(stage);
         stage.show();
     }
