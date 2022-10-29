0b29003deeb41908f5887a927cc2b3c65ecf6956
Maryam Bouchhar <meryem.bouchhar@gmail.com>
2022-10-29 11:55:40 +0100

    done

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/EmployeesController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/EmployeesController.java
@@ -5,6 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,10 +13,14 @@ import java.util.ResourceBundle;
 public class EmployeesController implements Initializable {
     @FXML
     private ComboBox<String> combo;
-    ObservableList<String> list= FXCollections.observableArrayList("PDF","Excel");
+    ObservableList<String> list = FXCollections.observableArrayList("PDF", "Excel");
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-  combo.setItems(list);
+        combo.setItems(list);
+    }
+
+    public void openAddEmployee(MouseEvent mouseEvent) {
+
     }
 }
