e78ef20ee82f4ace5a54a40c8cfbd7ebe416ed06
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-10-30 13:56:02 +0100

    add controllers

---
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/SalesController.java
@@ -0,0 +1,13 @@
+package com.dfms.dairy_farm_management_system.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SalesController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
