7de8b49c3b6acc0780370eb2715916d19124f891
Maryam Bouchhar <meryem.bouchhar@gmail.com>
2022-10-30 13:56:09 +0100

    Done!!

--- src/main/java/com/dfms/dairy_farm_management_system/models/User.java
+++ src/main/java/com/dfms/dairy_farm_management_system/models/User.java
@@ -1,4 +1,27 @@
 package com.dfms.dairy_farm_management_system.models;
 
 public class User {
+    private String password;
+    private int roleId;
+
+    public User(String password, int roleId) {
+        this.password = password;
+        this.roleId = roleId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
 }
