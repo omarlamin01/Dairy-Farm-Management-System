727281dc017a8403defee8b6b4a74c5a3f7594ba
Maryam Bouchhar <meryem.bouchhar@gmail.com>
2022-10-30 14:59:01 +0100

    done

---
+++ src/main/java/com/dfms/dairy_farm_management_system/models/Purchases.java
@@ -0,0 +1,47 @@
+package com.dfms.dairy_farm_management_system.models;
+
+public class Purchases {
+    private String firstName;
+    private String lastName;
+    private int phone ;
+    private String email;
+
+    public Purchases(String firstName, String lastName, int phone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmai(String email) {
+        this.email = email;
+    }
+}
