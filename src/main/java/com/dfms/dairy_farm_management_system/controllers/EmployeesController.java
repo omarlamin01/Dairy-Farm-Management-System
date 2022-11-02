a5bfd85cb4c5a4cba75d9770ed8a5a39b30ea7c0
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-02 16:58:11 +0100

    some changes

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/EmployeesController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/EmployeesController.java
@@ -7,7 +7,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.input.MouseEvent;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
