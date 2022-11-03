b39efca2d07e121eb3ce32a9e7ea98f79eed1d56
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-03 21:53:36 +0100

    fix search icon and add open the employee popup

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/EmployeesController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/EmployeesController.java
@@ -1,5 +1,6 @@
 package com.dfms.dairy_farm_management_system.controllers;
 
+import com.dfms.dairy_farm_management_system.helpers.Helper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -7,8 +8,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.input.MouseEvent;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static com.dfms.dairy_farm_management_system.helpers.Helper.openNewWindow;
 
 public class EmployeesController implements Initializable {
     @FXML
@@ -20,7 +24,7 @@ public class EmployeesController implements Initializable {
         combo.setItems(list);
     }
 
-    public void openAddEmployee(MouseEvent mouseEvent) {
-
+    public void openAddEmployee(MouseEvent mouseEvent) throws IOException {
+        openNewWindow("Add Employee", "add_new_employee");
     }
 }
