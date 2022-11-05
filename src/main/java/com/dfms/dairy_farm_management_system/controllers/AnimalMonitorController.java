cdb6bd4b8ac075f686994ece7f2dea0adcb62029
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-05 10:11:34 +0100

    Update AnimalMonitorController.java

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/AnimalMonitorController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/AnimalMonitorController.java
@@ -1,9 +1,13 @@
 package com.dfms.dairy_farm_management_system.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.scene.input.*;
+import static com.dfms.dairy_farm_management_system.helpers.Helper.openNewWindow;
 
 public class AnimalMonitorController implements Initializable {
     @Override
@@ -11,5 +15,8 @@ public class AnimalMonitorController implements Initializable {
 
     }
 
-    
+    @FXML
+    public void oppenAddHealthStatus(MouseEvent mouseEvent) throws IOException {
+        openNewWindow("Health status", "add_new_health_status");
+    }
 }
