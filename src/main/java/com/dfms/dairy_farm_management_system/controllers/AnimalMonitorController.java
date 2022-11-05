9d7fc2fe219a46e21d1b284c6119ea78198f4a07
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-05 09:42:34 +0100

    Update AnimalMonitorController.java

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/AnimalMonitorController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/AnimalMonitorController.java
@@ -1,4 +1,15 @@
 package com.dfms.dairy_farm_management_system.controllers;
 
-public class AnimalMonitorController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AnimalMonitorController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    
 }
