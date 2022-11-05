109ea2ea76e602a0d4e4b4646a99eae9386cf87a
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-05 14:46:07 +0100

    Update AnimalMonitorController.java

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/AnimalMonitorController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/AnimalMonitorController.java
@@ -6,31 +6,45 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.*;
 import static com.dfms.dairy_farm_management_system.helpers.Helper.openNewWindow;
 
 public class AnimalMonitorController implements Initializable {
 
+    //Health status tab
     @FXML
     TextField healthStatusSearch;
     @FXML
     Button healthSearchButton;
     @FXML
     Button newStatusButton;
+
+    //Pregnancy tab
     @FXML
     TextField pregnancySearch;
     @FXML
     Button pregnancySearchButton;
     @FXML
     Button newPregnancyButton;
+
+    //Vaccin tab
     @FXML
     TextField vaccinSearch;
     @FXML
     Button vaccinSearchButton;
     @FXML
     Button newVaccintionButton;
+
+    //Vaccin pop-up
+    @FXML
+    ComboBox<String> animalVaccin;
+    @FXML
+    ComboBox<String> vaccinId;
+    @FXML
+    DatePicker vaccinationDate;
+    @FXML
+    TextArea vaccinNotes;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
