c20a6213306215a6c3ad843c0259de832b1db509
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-05 17:50:07 +0100

    Create PregnancyController.java

---
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/PregnancyController.java
@@ -0,0 +1,48 @@
+package com.dfms.dairy_farm_management_system.controllers;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PregnancyController implements Initializable {
+
+    @FXML
+    ComboBox<String> cowPregnancyID;
+    @FXML
+    DatePicker pregnancyStartDate;
+    @FXML
+    TextArea pregnancyNotes;
+
+    ObservableList<String> cows;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.setCows();
+
+        //set cows list
+        cowPregnancyID.setItems(cows);
+    }
+
+    public void setCows() {
+        //get cows ids from db
+        this.cows = FXCollections.observableArrayList("cow-1", "cow-2", "cow-3");
+    }
+
+    @FXML
+    public void addPregnancy(MouseEvent mouseEvent) {
+        System.out.println("Pregnancy { " +
+                "Cow id: \"" + cowPregnancyID.getTypeSelector() + "\"," +
+                "Start date: \"" + pregnancyStartDate.getConverter() + "\"," +
+                "Notes: \"" + pregnancyNotes.getText() + "\" " +
+                "},"
+        );
+    }
+}
