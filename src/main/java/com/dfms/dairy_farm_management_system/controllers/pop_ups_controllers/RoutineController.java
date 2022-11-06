8096b36891f227a2549f126c2a3bd2fa33081c8d
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-06 07:52:18 +0100

    Update RoutineController.java

---
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/pop_ups_controllers/RoutineController.java
@@ -0,0 +1,13 @@
+package com.dfms.dairy_farm_management_system.controllers.pop_ups_controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RoutineController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+    }
+}
