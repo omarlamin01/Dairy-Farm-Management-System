153721b0ca6cf3ec3f506f6da28b9de99288a413
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-07 19:32:10 +0100

    add required fields to profile controller

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/ProfileController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/ProfileController.java
@@ -1,4 +1,40 @@
 package com.dfms.dairy_farm_management_system.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class ProfileController {
+    @FXML
+    private TextField address_input;
+
+    @FXML
+    private TextField cin_input;
+
+    @FXML
+    private TextField confirm_new_password_input;
+
+    @FXML
+    private TextField current_password_input;
+
+    @FXML
+    private TextField email_input;
+
+    @FXML
+    private TextField first_name_input;
+
+    @FXML
+    private TextField last_name_input;
+
+    @FXML
+    private TextField new_password_input;
+
+    @FXML
+    private TextField phone_input;
+
+    @FXML
+    private Button update_login_info_btn;
+
+    @FXML
+    private Button update_personal_info_btn;
 }
