d05c9bd7c0c0f877f2e8352c755f2fc25a13bad4
Maryam Bouchhar <meryem.bouchhar@gmail.com>
2022-11-09 19:52:40 +0100

    some changes

--- src/main/java/com/dfms/dairy_farm_management_system/models/Animal.java
+++ src/main/java/com/dfms/dairy_farm_management_system/models/Animal.java
@@ -2,21 +2,23 @@ package com.dfms.dairy_farm_management_system.models;
 
 import java.util.Date;
 
-public class Animal extends Model{
+public class Animal{
    private int id_animal;
-
+   private String type;
    private Date birth_date;
    private Date purchase_date;
-
-
+   private Date created_at;
+   private Date updated_at;
    private int id_routine;
    private int id_race;
 
-    public Animal(int id, Date birth_date, Date purchase_date,  int id_routine, int id_race) {
-        this.id_animal = id;
+    public Animal(int id_animal, String type, Date birth_date, Date purchase_date, Date created_at, Date updated_at, int id_routine, int id_race) {
+        this.id_animal = id_animal;
+        this.type = type;
         this.birth_date = birth_date;
         this.purchase_date = purchase_date;
-
+        this.created_at = created_at;
+        this.updated_at = updated_at;
         this.id_routine = id_routine;
         this.id_race = id_race;
     }
@@ -25,56 +27,67 @@ public class Animal extends Model{
 
     }
 
-    public void setId(int id) {
-        this.id_animal = id;
-    }
-
-
-
-    public void setBirth_date(Date birth_date) {
-        this.birth_date = birth_date;
-    }
-
-    public void setPurchase_date(Date purchase_date) {
-        this.purchase_date = purchase_date;
-    }
-
-
-    public void setId_routine(int id_routine) {
-        this.id_routine= id_routine;
-    }
-
-    public void setId_race(int id_race) {
-        this.id_race = id_race;
-    }
-
-
-
-
-
-    public int getId() {
+    public int getId_animal() {
         return id_animal;
     }
 
+    public void setId_animal(int id_animal) {
+        this.id_animal = id_animal;
+    }
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
     public Date getBirth_date() {
         return birth_date;
+    }
+
+    public void setBirth_date(Date birth_date) {
+        this.birth_date = birth_date;
     }
 
     public Date getPurchase_date() {
         return purchase_date;
     }
 
+    public void setPurchase_date(Date purchase_date) {
+        this.purchase_date = purchase_date;
+    }
 
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
 
     public int getId_routine() {
         return id_routine;
+    }
+
+    public void setId_routine(int id_routine) {
+        this.id_routine = id_routine;
     }
 
     public int getId_race() {
         return id_race;
     }
 
-
+    public void setId_race(int id_race) {
+        this.id_race = id_race;
+    }
 }
--- src/main/java/com/dfms/dairy_farm_management_system/models/User.java
+++ src/main/java/com/dfms/dairy_farm_management_system/models/User.java
@@ -6,25 +6,21 @@ public class User extends Employee{
     private int id_user;
     private String password;
     private int roleId;
-    @Override
-    public void setId(int id) {
-        this.id_user= id;
-    }
 
-    @Override
-    public int getId() {
-        return id_user;
-    }
-
-    public User(int id, String firstName, String lastName, char gender, String cin, String email, int phone, String adresse, int id1, float salary, Date recruitmentDate, String contractType, int id2, String password, int roleId) {
-        super(id, firstName, lastName, gender, cin, email, phone, adresse, id1, salary, recruitmentDate, contractType);
-        this.id_user = id2;
+    public User(int id_employee, String firstName, String lastName, char gender, String cin, String email, int phone, String adresse, float salary, Date recruitmentDate, String contractType, Date updated_at, Date created_at, int id_user, String password, int roleId) {
+        super(id_employee, firstName, lastName, gender, cin, email, phone, adresse, salary, recruitmentDate, contractType, updated_at, created_at);
+        this.id_user = id_user;
         this.password = password;
         this.roleId = roleId;
     }
 
+    public int getId_user() {
+        return id_user;
+    }
 
-
+    public void setId_user(int id_user) {
+        this.id_user = id_user;
+    }
 
     public String getPassword() {
         return password;
