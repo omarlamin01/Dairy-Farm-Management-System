f1728b5380bd15c9362f636edcf3dae38c80b788
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-10 21:11:53 +0100

    update employee controller

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/EmployeesController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/EmployeesController.java
@@ -7,9 +7,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.util.Callback;
 
 import java.io.IOException;
 import java.net.URL;
@@ -67,7 +72,7 @@ public class EmployeesController implements Initializable {
     //get all the employees
     public ObservableList<Employee> getEmployees() {
         ObservableList<Employee> employees = FXCollections.observableArrayList();
-        String query = "SELECT * FROM employees";
+        String query = "SELECT * FROM employee";
         try {
             st = con.createStatement();
             ResultSet rs = st.executeQuery(query);
@@ -94,7 +99,78 @@ public class EmployeesController implements Initializable {
         last_name_col.setCellValueFactory(new PropertyValueFactory<Employee, String>("last_name"));
         email_col.setCellValueFactory(new PropertyValueFactory<Employee, String>("email"));
         salary_col.setCellValueFactory(new PropertyValueFactory<Employee, String>("salary"));
-        actions_col.setCellValueFactory(new PropertyValueFactory<Employee, String>("actions"));
+        Callback<TableColumn<Employee, String>, TableCell<Employee, String>> cellFoctory = (TableColumn<Employee, String> param) -> {
+            final TableCell<Employee, String> cell = new TableCell<Employee, String>() {
+                Image edit_img = new Image(getClass().getResourceAsStream("/images/edit.png"));
+                final Button edit_btn = new Button();
+                Image delete_img = new Image(getClass().getResourceAsStream("/images/delete.png"));
+                final Button delete_btn = new Button();
+                Image view_details_img = new Image(getClass().getResourceAsStream("/images/eye.png"));
+                final Button view_details_btn = new Button();
+
+                @Override
+                protected void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+                    //that cell created only on non-empty rows
+                    if (empty) {
+                        setGraphic(null);
+                        setText(null);
+                    } else {
+                        view_details_btn.setStyle("-fx-background-color: transparent;-fx-cursor: hand;-fx-size:15px;");
+                        ImageView iv1 = new ImageView();
+                        iv1.setImage(view_details_img);
+                        iv1.setPreserveRatio(true);
+                        iv1.setSmooth(true);
+                        iv1.setCache(true);
+                        view_details_btn.setGraphic(iv1);
+
+                        setGraphic(view_details_btn);
+                        setText(null);
+
+
+                        edit_btn.setStyle("-fx-background-color: transparent;-fx-cursor: hand;-fx-size:15px;");
+                        ImageView iv = new ImageView();
+                        iv.setImage(edit_img);
+                        iv.setPreserveRatio(true);
+                        iv.setSmooth(true);
+                        iv.setCache(true);
+                        edit_btn.setGraphic(iv);
+
+                        setGraphic(edit_btn);
+                        setText(null);
+
+                        delete_btn.setStyle("-fx-background-color: transparent;-fx-cursor: hand;-fx-size:15px;");
+                        ImageView iv2 = new ImageView();
+
+                        iv2.setImage(delete_img);
+                        iv2.setPreserveRatio(true);
+                        iv2.setSmooth(true);
+                        iv2.setCache(true);
+                        delete_btn.setGraphic(iv2);
+
+
+                        setGraphic(delete_btn);
+
+                        setText(null);
+
+                        HBox managebtn = new HBox(edit_btn, delete_btn, view_details_btn);
+                        managebtn.setStyle("-fx-alignment:center");
+                        HBox.setMargin(edit_btn, new Insets(1, 1, 0, 3));
+                        HBox.setMargin(delete_btn, new Insets(1, 1, 0, 2));
+                        HBox.setMargin(view_details_btn, new Insets(1, 1, 0, 1));
+
+                        setGraphic(managebtn);
+                        setText(null);
+                        delete_btn.setOnMouseClicked((MouseEvent event) -> {
+                            System.out.println("delete");
+                        });
+                    }
+                }
+            };
+            return cell;
+        };
+        actions_col.setCellFactory(cellFoctory);
+        employees_table.setItems(employees);
     }
 
     public void openAddEmployee(MouseEvent mouseEvent) throws IOException {
