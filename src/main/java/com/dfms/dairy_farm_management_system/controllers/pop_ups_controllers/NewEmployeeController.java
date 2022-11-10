17b5a08b6553d8397be6393a6b69357cc4b92edd
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-10 22:43:16 +0100

    create method to add employee to the database

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/pop_ups_controllers/NewEmployeeController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/pop_ups_controllers/NewEmployeeController.java
@@ -1,22 +1,36 @@
 package com.dfms.dairy_farm_management_system.controllers.pop_ups_controllers;
 
+import com.dfms.dairy_farm_management_system.connection.DBConfig;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 import static com.dfms.dairy_farm_management_system.helpers.Helper.*;
 
 public class NewEmployeeController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.setGenderComboItems();
+        this.setContractComboItems();
+        this.setRoleComboItems();
+        validatePhoneInput(phoneNumberInput);
+        validateDecimalInput(salaryInput);
+    }
+
+    private Statement st;
+    private PreparedStatement pst;
+    private Connection con = DBConfig.getConnection();
     @FXML
     TextField lastNameInput;
     @FXML
@@ -41,14 +55,6 @@ public class NewEmployeeController implements Initializable {
     ObservableList<String> rolesList;
     @FXML
     private TextField cininput;
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        this.setGenderComboItems();
-        this.setContractComboItems();
-        this.setRoleComboItems();
-        validatePhoneInput(phoneNumberInput);
-        validateDecimalInput(salaryInput);
-    }
 
     public void setGenderComboItems() {
         this.genderCombo.setItems(FXCollections.observableArrayList("Male", "Female"));
@@ -66,21 +72,45 @@ public class NewEmployeeController implements Initializable {
     }
 
     @FXML
-    public void addEmployee(MouseEvent mouseEvent) {
-        System.out.println("Employee: { " +
-                "First name: \"" + this.firstNameInput.getText() + "\", " +
-                "Last name: \"" + this.lastNameInput.getText() + "\", " +
-                "Email: \"" + this.emailInput.getText() + "\", " +
-                "Phone: \"" + this.phoneNumberInput.getText() + "\", " +
-                "Adress: \"" + this.adressInput.getText() + "\", " +
-                "CIN: \"" + this.cininput.getText() + "\", " +
-                "Salary: \"" + this.salaryInput.getText() + "\", " +
-                "Hire date: \"" + this.hireDate.getValue() + "\", " +
-                "Contract type: \"" + this.contractCombo.getValue() + "\", " +
-                "Gender: \"" + this.genderCombo.getValue() + "\", " +
-                "Role: \"" + this.roleCombo.getValue() + "\"" +
-                " }");
+    public void addEmployee(MouseEvent mouseEvent) throws SQLException {
+        System.out.println("Employee: { " + "First name: \"" + this.firstNameInput.getText() + "\", " + "Last name: \"" + this.lastNameInput.getText() + "\", " + "Email: \"" + this.emailInput.getText() + "\", " + "Phone: \"" + this.phoneNumberInput.getText() + "\", " + "Adress: \"" + this.adressInput.getText() + "\", " + "CIN: \"" + this.cininput.getText() + "\", " + "Salary: \"" + this.salaryInput.getText() + "\", " + "Hire date: \"" + this.hireDate.getValue() + "\", " + "Contract type: \"" + this.contractCombo.getValue() + "\", " + "Gender: \"" + this.genderCombo.getValue() + "\", " + "Role: \"" + this.roleCombo.getValue() + "\"" + " }");
 
+        String firstName = this.firstNameInput.getText();
+        String lastName = this.lastNameInput.getText();
+        String email = this.emailInput.getText();
+        String phone = this.phoneNumberInput.getText();
+        String adress = this.adressInput.getText();
+        String cin = this.cininput.getText();
+        String salary = this.salaryInput.getText();
+        String hireDate = this.hireDate.getValue().toString();
+        String contractType = this.contractCombo.getValue();
+        String gender = this.genderCombo.getValue();
+        String role = this.roleCombo.getValue();
+
+        String query = "INSERT INTO employee (first_name, last_name, gender, cin, email, phone, adress, salary, recruitment_date, contract_type, role) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        try {
+            this.pst = this.con.prepareStatement(query);
+            this.pst.setString(1, firstName);
+            this.pst.setString(2, lastName);
+            this.pst.setString(3, gender);
+            this.pst.setString(4, cin);
+            this.pst.setString(5, email);
+            this.pst.setString(6, phone);
+            this.pst.setString(7, adress);
+            this.pst.setString(8, salary);
+            this.pst.setString(9, hireDate);
+            this.pst.setString(10, contractType);
+            this.pst.setString(11, role);
+            this.pst.execute();
+            this.pst.close();
+            this.con.close();
+            displayAlert("Done", "Employee added successfully", Alert.AlertType.INFORMATION);
+
+        } catch (SQLException e) {
+            displayAlert("Error", "Error while adding employee", Alert.AlertType.ERROR);
+            e.printStackTrace();
+        }
         ((Stage) (((Button) mouseEvent.getSource()).getScene().getWindow())).close();
     }
 }
