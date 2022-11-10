3c3ff9f115028c33c3c4d42cb2660c96f1ebe48f
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-10 20:16:41 +0100

    setup employees controller

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/EmployeesController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/EmployeesController.java
@@ -1,11 +1,11 @@
 package com.dfms.dairy_farm_management_system.controllers;
 
-import com.dfms.dairy_farm_management_system.helpers.Helper;
+import com.dfms.dairy_farm_management_system.models.Employee;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
@@ -17,11 +17,39 @@ import static com.dfms.dairy_farm_management_system.helpers.Helper.openNewWindow
 public class EmployeesController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        combo.setItems(list);
+        export_cb.setItems(list);
     }
 
     @FXML
-    private ComboBox<String> combo;
+    private TableView<Employee> employees_table;
+    @FXML
+    private TableColumn<Employee, String> actions_col;
+
+    @FXML
+    private TableColumn<Employee, String> col_id;
+
+    @FXML
+    private TableColumn<Employee, String> email_col;
+    @FXML
+    private TableColumn<Employee, String> first_name_col;
+
+    @FXML
+    private TableColumn<Employee, String> last_name_col;
+
+
+    @FXML
+    private TableColumn<Employee, String> salary_col;
+
+    @FXML
+    private Button search_btn;
+
+    @FXML
+    private TextField search_employee_input;
+    @FXML
+    private ComboBox<String> export_cb;
+
+    @FXML
+    private Button openAddNewEmployeeBtn;
     ObservableList<String> list = FXCollections.observableArrayList("PDF", "Excel");
 
     public void openAddEmployee(MouseEvent mouseEvent) throws IOException {
