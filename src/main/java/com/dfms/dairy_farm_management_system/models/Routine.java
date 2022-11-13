eece4c327648fc895fd92f1ff9d71e36d91a7998
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-13 19:37:49 +0100

    Update Routine.java

--- src/main/java/com/dfms/dairy_farm_management_system/models/Routine.java
+++ src/main/java/com/dfms/dairy_farm_management_system/models/Routine.java
@@ -2,23 +2,14 @@ package com.dfms.dairy_farm_management_system.models;
 
 import java.util.Date;
 
-public class Routine {
+public class Routine implements Model {
     private int id_routine;
-
     private String name;
     private String note;
-    public Routine(int id,String name, String note) {
-        this.id_routine = id;
-
-        this.name = name;
-        this.note = note;
-
-    }
 
     public void setId(int id) {
         this.id_routine = id;
     }
-
 
 
     public void setName(String name) {
@@ -30,11 +21,9 @@ public class Routine {
     }
 
 
-
     public int getId() {
         return id_routine;
     }
-
 
 
     public String getName() {
@@ -46,5 +35,18 @@ public class Routine {
     }
 
 
+    @Override
+    public boolean save() {
+        return false;
+    }
 
+    @Override
+    public boolean update() {
+        return false;
+    }
+
+    @Override
+    public boolean delete() {
+        return false;
+    }
 }
