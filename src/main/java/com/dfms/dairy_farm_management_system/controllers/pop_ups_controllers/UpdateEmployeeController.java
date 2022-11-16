299488b9f8bf7ce5362ea6aee00d2e3c64ff65ae
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-16 15:15:01 +0100

    add properties to controller

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/pop_ups_controllers/UpdateEmployeeController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/pop_ups_controllers/UpdateEmployeeController.java
@@ -1,4 +1,48 @@
 package com.dfms.dairy_farm_management_system.controllers.pop_ups_controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class UpdateEmployeeController {
+
+    @FXML
+    private TextField addressInput;
+
+    @FXML
+    private TextField cinInput;
+
+    @FXML
+    private ComboBox<?> contractCombo;
+
+    @FXML
+    private TextField emailInput;
+
+    @FXML
+    private TextField firstNameInput;
+
+    @FXML
+    private ComboBox<?> genderCombo;
+
+    @FXML
+    private DatePicker hireDate;
+
+    @FXML
+    private TextField lastNameInput;
+
+    @FXML
+    private TextField phoneNumberInput;
+
+    @FXML
+    private ComboBox<?> roleCombo;
+
+    @FXML
+    private TextField salaryInput;
+
+    @FXML
+    void updateEmployee(MouseEvent event) {
+
+    }
 }
