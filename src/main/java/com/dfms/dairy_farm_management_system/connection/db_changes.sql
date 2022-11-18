5a2b570a4d7d9adb88e81b523119f518c8cff9d9
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-18 21:54:29 +0100

    # Rename some tables, fields & fix some problems

--- src/main/java/com/dfms/dairy_farm_management_system/connection/db_changes.sql
+++ src/main/java/com/dfms/dairy_farm_management_system/connection/db_changes.sql
@@ -128,31 +128,139 @@ ALTER TABLE `user`
 SAVEPOINT H;
 
 ALTER TABLE `employee`
+    ADD PRIMARY KEY (`cin`);
+SAVEPOINT I;
+
+ALTER TABLE `employee`
     DROP `id`;
 COMMIT;
 
 # Unicity of some fields
-ALTER TABLE `client` ADD UNIQUE(`name`, `phone`, `email`);
+ALTER TABLE `client`
+    ADD UNIQUE (`name`, `phone`, `email`);
 SAVEPOINT A;
 
-ALTER TABLE `employee` ADD UNIQUE(`cin`, `email`, `phone`);
+ALTER TABLE `employee`
+    ADD UNIQUE (`cin`, `email`, `phone`);
 SAVEPOINT B;
 
-ALTER TABLE `race` ADD UNIQUE(`name`);
+ALTER TABLE `race`
+    ADD UNIQUE (`name`);
 SAVEPOINT C;
 
-ALTER TABLE `role` ADD UNIQUE(`name`);
+ALTER TABLE `role`
+    ADD UNIQUE (`name`);
 SAVEPOINT D;
 
-ALTER TABLE `routine` ADD UNIQUE(`name`);
+ALTER TABLE `routine`
+    ADD UNIQUE (`name`);
 SAVEPOINT E;
 
-ALTER TABLE `stock` ADD UNIQUE(`name`);
+ALTER TABLE `stock`
+    ADD UNIQUE (`name`);
 SAVEPOINT F;
 
-ALTER TABLE `supplier` ADD UNIQUE(`name`, `phone`, `email`);
+ALTER TABLE `supplier`
+    ADD UNIQUE (`name`, `phone`, `email`);
 SAVEPOINT G;
 
-ALTER TABLE `user` ADD UNIQUE(`cin`, `phone`, `email`);
+ALTER TABLE `user`
+    ADD UNIQUE (`cin`, `phone`, `email`);
 COMMIT;
 
+# Rename some tables, fields & fix some problems
+DROP TABLE `vaccine`;
+ALTER TABLE `animal` RENAME TO `animals`;
+ALTER TABLE `animal_sale` RENAME TO `animals_sales`;
+ALTER TABLE `client` RENAME TO `clients`;
+ALTER TABLE `employee` RENAME TO `employees`;
+ALTER TABLE `milk_collection` RENAME TO `milk_collections`;
+ALTER TABLE `milk_sale` RENAME TO `milk_sales`;
+ALTER TABLE `pregnancy` RENAME TO `pregnancies`;
+ALTER TABLE `purchase` RENAME TO `purchases`;
+ALTER TABLE `race` RENAME TO `races`;
+ALTER TABLE `role` RENAME TO `roles`;
+ALTER TABLE `routine` RENAME TO `routines`;
+ALTER TABLE `stock` RENAME TO `stocks`;
+ALTER TABLE `supplier` RENAME TO `suppliers`;
+ALTER TABLE `user` RENAME TO `users`;
+SAVEPOINT A;
+
+ALTER TABLE `animals`
+    CHANGE `routine_id` `routine` INT(11) NOT NULL,
+    CHANGE `race_id` `race` INT(11) NOT NULL;
+SAVEPOINT B;
+
+ALTER TABLE `animal_sale`
+    CHANGE `client_id` `client` INT(11) NOT NULL,
+    CHANGE `animal_id` `animal` INT(11) NULL DEFAULT NULL;
+SAVEPOINT C;
+
+ALTER TABLE `client`
+    CHANGE `phone` `phone` VARCHAR(20) NULL,
+    CHANGE `email` `email` VARCHAR(50) NULL;
+SAVEPOINT D;
+
+ALTER TABLE `employee`
+    CHANGE `address` `address` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    CHANGE `recruitment_date` `hire_date` DATE NOT NULL;
+SAVEPOINT E;
+
+ALTER TABLE `health_status`
+    CHANGE `animal_id` `animal` VARCHAR(11) NULL DEFAULT NULL,
+    CHANGE `breading` `breathing` INT NULL DEFAULT '0';
+SAVEPOINT F;
+
+ALTER TABLE `health_status`
+    DROP INDEX `fk_health_status_id_vaccine`;
+SAVEPOINT G;
+
+ALTER TABLE `health_status`
+    DROP `vaccine_id`;
+SAVEPOINT H;
+
+ALTER TABLE `pregnancy`
+    DROP `pregnancy_type`;
+SAVEPOINT I;
+
+ALTER TABLE `pregnancy`
+    CHANGE `pregnancy_status` `pregnancy_status` ENUM ('Ongoing','finished','failed') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+SAVEPOINT J;
+
+ALTER TABLE `routine`
+    CHANGE `note` `note` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+SAVEPOINT K;
+
+ALTER TABLE `supplier`
+    CHANGE `phone` `phone` VARCHAR(20) NOT NULL,
+    CHANGE `email` `email` VARCHAR(50) NOT NULL;
+SAVEPOINT L;
+
+ALTER TABLE `user`
+    CHANGE `role_id` `role` INT(11) NOT NULL,
+    CHANGE `password` `password` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+SAVEPOINT M;
+
+ALTER TABLE `user`
+    CHANGE `created_at` `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `address`;
+ALTER TABLE `user`
+    CHANGE `first_name` `first_name` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `id`;
+ALTER TABLE `user`
+    CHANGE `last_name` `last_name` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `first_name`;
+ALTER TABLE `user`
+    CHANGE `cin` `cin` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `last_name`;
+ALTER TABLE `user`
+    CHANGE `email` `email` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `cin`;
+ALTER TABLE `user`
+    CHANGE `password` `password` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `email`;
+ALTER TABLE `user`
+    CHANGE `updated_at` `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `address`;
+ALTER TABLE `user`
+    CHANGE `gender` `gender` ENUM ('M','F') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `password`;
+ALTER TABLE `user`
+    CHANGE `phone` `phone` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `gender`;
+ALTER TABLE `user`
+    CHANGE `salary` `salary` FLOAT NOT NULL AFTER `phone`;
+ALTER TABLE `user`
+    CHANGE `address` `address` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `salary`;
+COMMIT;
