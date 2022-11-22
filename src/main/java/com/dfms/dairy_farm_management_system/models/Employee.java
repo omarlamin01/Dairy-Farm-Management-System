71cb2f4649d29c40ab025afa570dd7c1be33483d
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-22 22:39:16 +0100

    Update Employee.java

--- src/main/java/com/dfms/dairy_farm_management_system/models/Employee.java
+++ src/main/java/com/dfms/dairy_farm_management_system/models/Employee.java
@@ -157,8 +157,7 @@ public class Employee implements Model {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
 
-        String updateQuery = "UPDATE `employees` SET " +
-                "`first_name` = '" + first_name +
+        String updateQuery = "UPDATE `employees` SET `first_name` = '" + first_name +
                 "', `last_name` = '" + last_name +
                 "', `gender` = '" + (gender.equalsIgnoreCase("Male") ? 'M' : 'F') +
                 "', `cin` = '" + cin +
@@ -169,7 +168,7 @@ public class Employee implements Model {
                 "', `recruitment_date` = '" + "2022-11-12" +
                 "', `contract_type` = '" + contract_type +
                 "', `updated_at` = '" + dtf.format(now) + "' " +
-                "WHERE `employees`.`cin` = '" + cin + "'";
+                "WHERE cin = '" + cin + "'";
         try {
             Connection connection = DBConfig.getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement(updateQuery);
