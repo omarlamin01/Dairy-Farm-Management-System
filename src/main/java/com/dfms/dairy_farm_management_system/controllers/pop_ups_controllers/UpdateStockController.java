359ae918cbdad5bc32bbc729561c10c058f4fa08
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-23 00:20:43 +0100

    Update UpdateStockController.java

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/pop_ups_controllers/UpdateStockController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/pop_ups_controllers/UpdateStockController.java
@@ -3,7 +3,7 @@ package com.dfms.dairy_farm_management_system.controllers.pop_ups_controllers;
 import com.dfms.dairy_farm_management_system.models.Stock;
 
 public class UpdateStockController {
-    public void fetchEmployee(Stock product) {
+    public void fetchProduct(Stock product) {
 
     }
 }
