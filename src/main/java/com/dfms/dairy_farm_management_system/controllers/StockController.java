46ed3845a260c75696168e5f4b0d563abfe472b4
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-22 23:28:51 +0100

    Update StockController.java

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/StockController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/StockController.java
@@ -2,8 +2,7 @@ package com.dfms.dairy_farm_management_system.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
@@ -18,6 +17,36 @@ public class StockController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+    @FXML
+    private TableColumn<?, ?> actions_col;
+
+    @FXML
+    private TableColumn<?, ?> added_date_col;
+
+    @FXML
+    private TableColumn<?, ?> availability_col;
+
+    @FXML
+    private ComboBox<?> combo;
+
+    @FXML
+    private TableColumn<?, ?> id_col;
+
+    @FXML
+    private Button openAddNewEmployeeBtn;
+
+    @FXML
+    private TableColumn<?, ?> product_name_col;
+
+    @FXML
+    private TableColumn<?, ?> product_type_col;
+
+    @FXML
+    private TextField search_stock_input;
+
+    @FXML
+    private TableView<?> stock_table;
 
     @FXML
     void openAddProduct(MouseEvent event) throws IOException {
