232a12dc33cc79d52d4c97b8a042d146b843167a
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-23 17:09:48 +0100

    Update NewProductController.java

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/pop_ups_controllers/NewProductController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/pop_ups_controllers/NewProductController.java
@@ -4,6 +4,7 @@ import com.dfms.dairy_farm_management_system.models.Availability;
 import com.dfms.dairy_farm_management_system.models.Stock;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
@@ -14,7 +15,7 @@ import java.net.URL;
 import java.util.Random;
 import java.util.ResourceBundle;
 
-import static com.dfms.dairy_farm_management_system.helpers.Helper.validateNumericInput;
+import static com.dfms.dairy_farm_management_system.helpers.Helper.*;
 
 public class NewProductController implements Initializable {
     @Override
@@ -37,6 +38,11 @@ public class NewProductController implements Initializable {
 
     @FXML
     public void addProduct(MouseEvent mouseEvent) {
+        if (inputsAreEmpty()) {
+            displayAlert("Error", "Please fill all the fields", Alert.AlertType.ERROR);
+            return;
+        }
+
         Stock product = new Stock();
         product.setId(new Random().nextInt());
         product.setName(this.product_name.getText());
@@ -52,11 +58,24 @@ public class NewProductController implements Initializable {
         }
 
         product.setUnit(this.mesure_unit_combo.getValue());
+
+        //save product to database
+        if (product.save()) {
+            displayAlert("Success", "Product added successfully", Alert.AlertType.INFORMATION);
+            closeWindow(mouseEvent);
+        } else {
+            displayAlert("Error", "An error occurred while adding the product", Alert.AlertType.ERROR);
+        }
+
         System.out.println(product.toString());
     }
 
     public void initializeComboBoxes() {
         this.product_type_combo.getItems().addAll("Machine", "Vaccine", "Feed", "Drug");
         this.mesure_unit_combo.getItems().addAll("Kg", "Litre", "Unit");
+    }
+
+    public boolean inputsAreEmpty() {
+        return this.product_name.getText().isEmpty() || this.product_quantity.getText().isEmpty() || this.mesure_unit_combo.getValue().isEmpty() || this.product_type_combo.getValue().isEmpty();
     }
 }
