c3a999a16de885c7383b68fcfb9e7cac5886d6dd
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-11-23 16:37:58 +0100

    Update db_changes.sql

--- src/main/java/com/dfms/dairy_farm_management_system/connection/db_changes.sql
+++ src/main/java/com/dfms/dairy_farm_management_system/connection/db_changes.sql
@@ -1,60 +1,11 @@
 use dairyfarm;
 
-# Rename some tables, fields & fix some problems
-ALTER TABLE `clients`
-    CHANGE `phone` `phone` VARCHAR(20) NULL,
-    CHANGE `email` `email` VARCHAR(50) NULL;
-SAVEPOINT D;
+# change address type in employee table
+ALTER TABLE employees
+    MODIFY address VARCHAR(255) NOT NULL;
 
-ALTER TABLE `employees`
-    CHANGE `address` `address` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-    CHANGE `recruitment_date` `hire_date` DATE NOT NULL;
-SAVEPOINT E;
-
-ALTER TABLE `pregnancies`
-    DROP `pregnancy_type`;
-SAVEPOINT I;
-
-ALTER TABLE `pregnancies`
-    CHANGE `pregnancy_status` `pregnancy_status` ENUM ('Ongoing','finished','failed') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
-SAVEPOINT J;
-
-ALTER TABLE `routines`
-    CHANGE `note` `note` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
-SAVEPOINT K;
-
-ALTER TABLE `suppliers`
-    CHANGE `phone` `phone` VARCHAR(20) NOT NULL,
-    CHANGE `email` `email` VARCHAR(50) NOT NULL;
-SAVEPOINT L;
-
-ALTER TABLE `users`
-    CHANGE `role_id` `role` INT(11) NOT NULL,
-    CHANGE `password` `password` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
-SAVEPOINT M;
-
-ALTER TABLE `users`
-    CHANGE `created_at` `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `address`;
-ALTER TABLE `users`
-    CHANGE `first_name` `first_name` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `id`;
-ALTER TABLE `users`
-    CHANGE `last_name` `last_name` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `first_name`;
-ALTER TABLE `users`
-    CHANGE `cin` `cin` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `last_name`;
-ALTER TABLE `users`
-    CHANGE `email` `email` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `cin`;
-ALTER TABLE `users`
-    CHANGE `password` `password` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `email`;
-ALTER TABLE `users`
-    CHANGE `updated_at` `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `address`;
-ALTER TABLE `users`
-    CHANGE `gender` `gender` ENUM ('M','F') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `password`;
-ALTER TABLE `users`
-    CHANGE `phone` `phone` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `gender`;
-ALTER TABLE `users`
-    CHANGE `salary` `salary` FLOAT NOT NULL AFTER `phone`;
-ALTER TABLE `users`
-    CHANGE `address` `address` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `salary`;
-COMMIT;
-
-
+# add quantity and availability columns to stock table
+ALTER TABLE stocks
+    ADD quantity INT NOT NULL;
+ALTER TABLE stocks
+    ADD availability ENUM ('available', 'out of stock') NOT NULL;
