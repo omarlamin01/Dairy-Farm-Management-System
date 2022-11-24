811ad7f0d36467c51261402831e0e7c8e6d6a3f0
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-11-24 13:16:06 +0100

    Update RoutineDetails.java

--- src/main/java/com/dfms/dairy_farm_management_system/models/RoutineDetails.java
+++ src/main/java/com/dfms/dairy_farm_management_system/models/RoutineDetails.java
@@ -1,59 +1,118 @@
 package com.dfms.dairy_farm_management_system.models;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.Date;
 
-public class RoutineDetails {
-    private  int id_routinedetail;
-    private int id_routine;
-    private int id_stock;
-    private float quantity;
-    private Date feed_timing;
+import static com.dfms.dairy_farm_management_system.connection.DBConfig.getConnection;
 
-    public RoutineDetails(int id, int id_routine, int id_stock, float quantity, Date feed_timing) {
-        this.id_routinedetail = id;
-        this.id_routine = id_routine;
-        this.id_stock = id_stock;
-        this.quantity = quantity;
-        this.feed_timing = feed_timing;
-    }
+public class RoutineDetails implements Model{
+    private int id;
+    private int stock_id;
+    private int routine_id;
+    private String feeding_time;
+    private Timestamp created_at;
+    private Timestamp updated_at;
 
     public int getId() {
-        return id_routinedetail;
+        return id;
     }
 
     public void setId(int id) {
-        this.id_routinedetail = id;
+        this.id = id;
     }
 
-    public int getId_routine() {
-        return id_routine;
+    public int getStock_id() {
+        return stock_id;
     }
 
-    public void setId_routine(int id_routine) {
-        this.id_routine = id_routine;
+    public void setStock_id(int stock_id) {
+        this.stock_id = stock_id;
     }
 
-    public int getId_stock() {
-        return id_stock;
+    public int getRoutine_id() {
+        return routine_id;
     }
 
-    public void setId_stock(int id_stock) {
-        this.id_stock = id_stock;
+    public void setRoutine_id(int routine_id) {
+        this.routine_id = routine_id;
     }
 
-    public float getQuantity() {
-        return quantity;
+    public String getFeeding_time() {
+        return feeding_time;
     }
 
-    public void setQuantity(float quantity) {
-        this.quantity = quantity;
+    public void setFeeding_time(String feeding_time) {
+        this.feeding_time = feeding_time;
     }
 
-    public Date getFeed_timing() {
-        return feed_timing;
+    public Timestamp getCreated_at() {
+        return created_at;
     }
 
-    public void setFeed_timing(Date feed_timing) {
-        this.feed_timing = feed_timing;
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    @Override
+    public boolean save() {
+        created_at = Timestamp.valueOf(LocalDateTime.now());
+        updated_at = Timestamp.valueOf(LocalDateTime.now());
+        String query = "INSERT INTO `routine_has_feeds` (stock_id, routine_id, feeding_time, created_at, updated_at) VALUES (?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement statement = getConnection().prepareStatement(query);
+
+            statement.setInt(1, stock_id);
+            statement.setInt(2, routine_id);
+            statement.setString(3, feeding_time);
+            statement.setTimestamp(4, created_at);
+            statement.setTimestamp(5, updated_at);
+
+            return statement.executeUpdate() != 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean update() {
+        updated_at = Timestamp.valueOf(LocalDateTime.now());
+        String query = "UPDATE `routine_has_feeds` SET stock_id = ?, routine_id = ?, feeding_time = ?, updated_at = ? WHERE id = " + id;
+        try {
+            PreparedStatement statement = getConnection().prepareStatement(query);
+
+            statement.setInt(1, stock_id);
+            statement.setInt(2, routine_id);
+            statement.setString(3, feeding_time);
+            statement.setTimestamp(4, updated_at);
+
+            return statement.executeUpdate() != 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete() {
+        String query = "DELETE FROM `routine_has_feeds` WHERE id = " + id;
+        try {
+            PreparedStatement statement = getConnection().prepareStatement(query);
+            return statement.executeUpdate() != 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
