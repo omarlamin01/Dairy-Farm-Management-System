bfe7b2aa5f75328d69ac3dfc2bee7311c2e4ea61
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-12-02 20:20:23 +0100

    install junit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,7 +11,6 @@ module com.dfms.dairy_farm_management_system {
     requires slf4j.log4j12;
     requires org.apache.logging.log4j;
     requires java.desktop;
-    requires org.junit.jupiter.api;
 
     opens com.dfms.dairy_farm_management_system to javafx.fxml;
     exports com.dfms.dairy_farm_management_system;
---
+++ src/test/com/dfms/dairy_farm_management_system/controllers/DashboardControllerTest.java
@@ -0,0 +1,13 @@
+package com.dfms.dairy_farm_management_system.controllers;
+
+import junit.framework.TestCase;
+
+public class DashboardControllerTest extends TestCase {
+
+    public void testGetSalesOfSpecificDay() {
+        assertEquals(0, 0);
+    }
+
+    public void testGetEarningsOfSpecificDay() {
+    }
+}
