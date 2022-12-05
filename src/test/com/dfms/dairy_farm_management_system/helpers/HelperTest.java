e96d132bb8a7944704513ec46392a6b75467c026
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-12-05 11:55:15 +0100

    Create HelperTest.java

---
+++ src/test/com/dfms/dairy_farm_management_system/helpers/HelperTest.java
@@ -0,0 +1,13 @@
+package com.dfms.dairy_farm_management_system.helpers;
+
+import junit.framework.TestCase;
+
+public class HelperTest extends TestCase {
+
+    public void testEncryptPassword() {
+    }
+
+    public void testMD5() {
+        
+    }
+}
