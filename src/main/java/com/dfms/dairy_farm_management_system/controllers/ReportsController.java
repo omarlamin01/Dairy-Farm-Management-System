9572f0967b8500d4de62585a122be2a3fd2d0bd6
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-12-09 11:19:09 +0100

    display table

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/ReportsController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/ReportsController.java
@@ -1,17 +1,26 @@
 package com.dfms.dairy_farm_management_system.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.css.Style;
+import javafx.css.Stylesheet;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.DateCell;
-import javafx.scene.control.DatePicker;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
+import org.openxmlformats.schemas.spreadsheetml.x2006.main.StyleSheetDocument;
 
+import javax.swing.text.html.StyleSheet;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.*;
 import java.time.LocalDate;
 import java.time.chrono.Chronology;
 import java.util.ResourceBundle;
+
+import static com.dfms.dairy_farm_management_system.connection.DBConfig.getConnection;
 
 public class ReportsController implements Initializable {
 //  milk collection part
@@ -27,6 +36,13 @@ public class ReportsController implements Initializable {
     @FXML
     private VBox milk_collection_results_area;
 
+    private class DailyMilkCollection {
+        Date collection_date;
+        float total_day_collection;
+        float morning_collection;
+        float evening_collection;
+    }
+
     /**
      * Called to initialize a controller after its root element has been
      * completely processed.
@@ -38,6 +54,7 @@ public class ReportsController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         initView();
+        search_btn.setOnMouseClicked(event -> { displayData(); });
     }
 
     private void initView() {
@@ -103,5 +120,68 @@ public class ReportsController implements Initializable {
             });
         });
 
+    }
+
+    private ObservableList<DailyMilkCollection> getData() {
+        LocalDate min_date = from_date_picker.getValue();
+        LocalDate max_date = to_date_picker.getValue();
+
+        ObservableList<DailyMilkCollection> data = FXCollections.observableArrayList();
+
+        try {
+            String query =
+                    "SELECT `created_at` AS collection_date, " +
+                    "sum(`quantity`) AS total_day_collection " +
+                    "FROM `milk_collections` WHERE `created_at` <= ? AND `created_at` >= ? " +
+                    "ORDER BY `created_at`";
+
+            PreparedStatement statement = getConnection().prepareStatement(query);
+
+            statement.setTimestamp(1, Timestamp.valueOf(max_date.atStartOfDay()));
+            statement.setTimestamp(2, Timestamp.valueOf(min_date.atStartOfDay()));
+
+            System.out.println(statement);
+            System.out.println(query);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                DailyMilkCollection dailyMilkCollection = new DailyMilkCollection();
+
+                dailyMilkCollection.collection_date = resultSet.getDate("collection_date");
+                dailyMilkCollection.total_day_collection = resultSet.getFloat("total_day_collection");
+//                dailyMilkCollection.morning_collection = resultSet.getFloat("morning_collection");
+//                dailyMilkCollection.evening_collection = resultSet.getFloat("evening_collection");
+
+                data.add(dailyMilkCollection);
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getSQLState());
+        }
+        return data;
+    }
+
+    private void displayData() {
+        milk_collection_results_area.getChildren().clear();
+        TableColumn<DailyMilkCollection, String> collection_date = new TableColumn<>("Date");
+        collection_date.setCellValueFactory(new PropertyValueFactory<DailyMilkCollection, String>("collection_date"));
+
+        TableColumn<DailyMilkCollection, String> total_day_collection = new TableColumn<>("Total day collection");
+        total_day_collection.setCellValueFactory(new PropertyValueFactory<DailyMilkCollection, String>("total_day_collection"));
+
+        TableColumn<DailyMilkCollection, String> morning_collection = new TableColumn<>("Morning collection");
+        morning_collection.setCellValueFactory(new PropertyValueFactory<DailyMilkCollection, String>("morning_collection"));
+
+        TableColumn<DailyMilkCollection, String> evening_collection = new TableColumn<>("Evening collection");
+        evening_collection.setCellValueFactory(new PropertyValueFactory<DailyMilkCollection, String>("evening_collection"));
+
+        TableView<DailyMilkCollection> data_table = new TableView<>();
+        data_table.getColumns().addAll(collection_date, total_day_collection, morning_collection, evening_collection);
+        data_table.getStylesheets().add("/style/table_view.css");
+        data_table.getStyleClass().add("table-view");
+        data_table.setItems(getData());
+        milk_collection_results_area.getChildren().add(data_table);
     }
 }
