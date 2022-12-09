34c1c05d64e913d115b2474ff57cee853abe9f51
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-12-09 12:08:30 +0100

    create settings controller

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/SettingsController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/SettingsController.java
@@ -1,13 +1,59 @@
 package com.dfms.dairy_farm_management_system.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 public class SettingsController {
-    public void refreshList(MouseEvent mouseEvent) {
+
+    @FXML
+    private TableColumn<?, ?> actions_col;
+
+    @FXML
+    private TableColumn<?, ?> added_date_col;
+
+    @FXML
+    private TableColumn<?, ?> id_col;
+
+    @FXML
+    private Button openAddNewEmployeeBtn;
+
+    @FXML
+    private Button openAddNewEmployeeBtn1;
+
+    @FXML
+    private ImageView refresh_table_table;
+
+    @FXML
+    private TableColumn<?, ?> role_name_col;
+
+    @FXML
+    private TextField search_stock_input;
+
+    @FXML
+    private TableView<?> stock_table;
+
+    @FXML
+    void openAddRole(MouseEvent event) {
 
     }
 
-    public void openAddRole(MouseEvent mouseEvent) {
+    @FXML
+    void openNewRole(MouseEvent event) {
+
+    }
+
+    @FXML
+    void refreshList(MouseEvent event) {
+
+    }
+
+    @FXML
+    void refreshTable(MouseEvent event) {
 
     }
 }
