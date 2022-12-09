1a842ab07a4879edd8ea5dfcaf9a4ff99b824a6d
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-12-09 12:11:20 +0100

    Update SettingsController.java

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/SettingsController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/SettingsController.java
@@ -1,5 +1,6 @@
 package com.dfms.dairy_farm_management_system.controllers;
 
+import com.dfms.dairy_farm_management_system.models.Role;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
@@ -11,13 +12,13 @@ import javafx.scene.input.MouseEvent;
 public class SettingsController {
 
     @FXML
-    private TableColumn<?, ?> actions_col;
+    private TableColumn<Role, String> actions_col;
 
     @FXML
-    private TableColumn<?, ?> added_date_col;
+    private TableColumn<Role, String> added_date_col;
 
     @FXML
-    private TableColumn<?, ?> id_col;
+    private TableColumn<Role, String> id_col;
 
     @FXML
     private Button openAddNewEmployeeBtn;
@@ -29,13 +30,13 @@ public class SettingsController {
     private ImageView refresh_table_table;
 
     @FXML
-    private TableColumn<?, ?> role_name_col;
+    private TableColumn<Role, String> role_name_col;
 
     @FXML
     private TextField search_stock_input;
 
     @FXML
-    private TableView<?> stock_table;
+    private TableView<Role> stock_table;
 
     @FXML
     void openAddRole(MouseEvent event) {
