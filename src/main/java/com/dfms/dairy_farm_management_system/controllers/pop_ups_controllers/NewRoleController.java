7c5b078984b542c8352782c6c50929c449bb6de5
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-12-09 12:17:06 +0100

    some changes

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/pop_ups_controllers/NewRoleController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/pop_ups_controllers/NewRoleController.java
@@ -1,4 +1,23 @@
 package com.dfms.dairy_farm_management_system.controllers.pop_ups_controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class NewRoleController {
+
+    @FXML
+    private Label Add_Update;
+
+    @FXML
+    private Label Head;
+
+    @FXML
+    private TextField role_name;
+
+    @FXML
+    void addNewRole(MouseEvent event) {
+
+    }
 }
