2b8afc2eef27ed1f9470e3950789ea265eeb0452
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2023-01-05 12:25:52 +0100

    remove unused coments

--- src/main/java/com/dfms/dairy_farm_management_system/controllers/DashboardController.java
+++ src/main/java/com/dfms/dairy_farm_management_system/controllers/DashboardController.java
@@ -174,30 +174,6 @@ public class DashboardController implements Initializable {
 
     //fill BarChart with sales of each day
     public void fillBarChart() {
-//        xAxis.setLabel("Programming Language");
-//
-//        yAxis.setLabel("Percent");
-//
-//        // Series 1 - Data of 2014
-//        XYChart.Series<String, Number> dataSeries1 = new XYChart.Series<String, Number>();
-//        dataSeries1.setName("2014");
-//
-//        dataSeries1.getData().add(new XYChart.Data<String, Number>("Java", 20.973));
-//        dataSeries1.getData().add(new XYChart.Data<String, Number>("C#", 4.429));
-//        dataSeries1.getData().add(new XYChart.Data<String, Number>("PHP", 2.792));
-//
-//        // Series 2 - Data of 2015
-//        XYChart.Series<String, Number> dataSeries2 = new XYChart.Series<String, Number>();
-//        dataSeries2.setName("2015");
-//
-//        dataSeries2.getData().add(new XYChart.Data<String, Number>("Java", 26.983));
-//        dataSeries2.getData().add(new XYChart.Data<String, Number>("C#", 6.569));
-//        dataSeries2.getData().add(new XYChart.Data<String, Number>("PHP", 6.619));
-//
-//        // Add Series to BarChart.
-//        barChart.getData().add(dataSeries1);
-//        barChart.getData().add(dataSeries2);
-
         xAxis.setLabel("Days");
         yAxis.setLabel("Sales");
 
